258f37f0934227c0204e809fc9ffac416b29b708
Leandro <lsf.falcao@gmail.com>
2014-12-19 18:01:09 -0300

    kaio e eu

--- java.js
+++ java.js
@@ -1,12 +1,12 @@
-/* var fight = function (atk, arm){
+ var fight = function (atk, arm){
     'use strict';
     if(atk > arm) {
         return 'first';
     } else {
-        return 'secund';
+        return 'second';
     }
-}
-
+};
+/*
 exercio 2
 
 //lop da batalha atributos devem ser aleatorio
@@ -26,81 +26,72 @@ if(life1>0) {return (vencedor é jogador 1)}
 else    {return (vencedor jogador 2)}
 }
 
-exercicio 3
+exercicio 3*/
 
-var p1 = (nome: Leandro, forca, armadura, resistencia, life){
-    'use strict';
-    jogador array (nome, forca, armadura, resistencia, life);
-    valor=10;
-    forca = Math.ceil(Math.random()*5);
-    valor= valor-força;
-    armadura = Math.ceil(Math.random()*5);
-    valor= valor-armadura;
+var person = function(nome){
+    var atributos = {Name: nome, forca: 0, armadura: 0, resistencia: 0, life: 0} 
+    valor = 10;
+    atributos.forca = Math.ceil(Math.random() * 5);
+    valor -= atributos.forca;
+    atributos.armadura = Math.ceil(Math.random() * 5);
+    valor -= atributos.armadura;
     
-    if(valor<5){
-    resistencia = valor;
+    if(valor < 5){
+    atributos.resistencia = valor;
     }
     else{
-    resistencia = Math.ceil(Math.random()*5);
+    atributos.resistencia = Math.ceil(Math.random() * 5);
     }
     
-    life = resistencia * 5;
-    if(valor=0){
-        life=1;
+    atributos.life = atributos.resistencia * 5;
+    if(valor = 0 ){
+        atributos.life = 1;
     }
+    return atributos.life;
+};
+
+//exercicio 4 (2 personagens)
+
+//var person2 = function(nome){
+//    var atributos2 = {Name2: nome, forca2: 0, armadura2: 0, resistencia2: 0, life2: 0} 
+//    valor = 10;
+//    atributos2.forca2 = Math.ceil(Math.random() * 5);
+//    valor -= atributos2.forca2;
+//    atributos2.armadura2 = Math.ceil(Math.random() * 5);
+//    valor -= atributos2.armadura2;
+//    
+//    if(valor < 5){
+//    atributos2.resistencia2 = valor;
+//    }
+//    else{
+//    atributos2.resistencia2 = Math.ceil(Math.random() * 5);
+//    }
+//    
+//    atributos2.life2 = atributos2.resistencia2 * 5;
+//    if(valor = 0 ){
+//        atributos2.life2 = 1;
+//    }
+//    return atributos2;
+//};
+
+
+var batalha = function(person, person2){
+
+    while(person2.life < 0 && person.life < 0){
+    life2 = life2 - (forca * (random(1,6)) - armadura2 * (random(1,6)));
+        if(life2>0)
+            life = life - (forca2 * (random(1,6)) - armadura * (random(1,6)));
+    }
+if(life2>0)
+    {return person.Name;}
+else
+    {return person2.Name;}
 }
 
-exercicio 4 (2 personagens)
 
+var character = document.querySelector('div'),
+    nome = character.querySelector('#nome');
 
-var p1 = (nome: Leandro){
-    'use strict';
-    valor=10;
-    forca1 = Math.ceil(Math.random()*5);
-    valor= valor-força1;
-    armadura1 = Math.ceil(Math.random()*5);
-    valor= valor-armadura1;
-    
-    if(valor<5){
-    resistencia1 = valor;
-    }
-    else{
-    resistencia1 = Math.ceil(Math.random()*5);
-    }
-    
-    life1 = resistencia1 * 5;
-    if(valor=0){
-        life1=1;
-    }
-}
-var p2 = (nome:caio){
-    valor=10;
-    forca = Math.ceil(Math.random()*5);
-    valor= valor-força;
-    armadura = Math.ceil(Math.random()*5);
-    valor= valor-armadura;
-    
-    if(valor<5){
-    resistencia = valor;
-    }
-    else{
-    resistencia = Math.ceil(Math.random()*5);
-    }
-    
-    life = resistencia * 5;
-    if(valor=0){
-        life=1;
-    }
-}
+    nome.innerHTML = batalha();
 
-fight = function (p1,p2){
-    while(life1<0 && life<0)
-{
-  life1= life1 -(forca*(random(1,6) - armadura1*(random(1,6));
-  if(lefi1>0){
-      life= life -(forca1*(random(1,6) - armadura*(random(1,6));
-  }
- }
-if(life1>0) {return p1;}
-else    {return p2;}
-}*/
+batalha(person('Kaio'), person('Léo'));
