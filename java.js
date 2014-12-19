3e03a639bf952525ad5e7857b27daf5bca5104cf
Leandro <lsf.falcao@gmail.com>
2014-12-19 13:37:05 -0300

    finalizando exercicios

---
+++ java.js
@@ -0,0 +1,106 @@
+/* var fight = function (atk, arm){
+    'use strict';
+    if(atk > arm) {
+        return 'first';
+    } else {
+        return 'secund';
+    }
+}
+
+exercio 2
+
+//lop da batalha atributos devem ser aleatorio
+//(totaldamage += Math.ceil(Math.random()*6);
+jogador1 Array(força1, armadura1, resistencia1, life1=5 * resistencia)
+jogador2 Array(força, armadura, resistencia, life=5 * resistencia)
+
+var fight = function (forca1, armadura1, resistencia1,life1,forca, armadura, resistencia,life){
+while(life1<0 && life<0)
+{
+  life1= life1 -(forca*(random(1,6) - armadura1*(random(1,6));
+  if(lefi1>0){
+  life= life -(forca1*(random(1,6) - armadura*(random(1,6));
+  }
+}
+if(life1>0) {return (vencedor é jogador 1)}
+else    {return (vencedor jogador 2)}
+}
+
+exercicio 3
+
+var p1 = (nome: Leandro, forca, armadura, resistencia, life){
+    'use strict';
+    jogador array (nome, forca, armadura, resistencia, life);
+    valor=10;
+    forca = Math.ceil(Math.random()*5);
+    valor= valor-força;
+    armadura = Math.ceil(Math.random()*5);
+    valor= valor-armadura;
+    
+    if(valor<5){
+    resistencia = valor;
+    }
+    else{
+    resistencia = Math.ceil(Math.random()*5);
+    }
+    
+    life = resistencia * 5;
+    if(valor=0){
+        life=1;
+    }
+}
+
+exercicio 4 (2 personagens)
+
+
+var p1 = (nome: Leandro){
+    'use strict';
+    valor=10;
+    forca1 = Math.ceil(Math.random()*5);
+    valor= valor-força1;
+    armadura1 = Math.ceil(Math.random()*5);
+    valor= valor-armadura1;
+    
+    if(valor<5){
+    resistencia1 = valor;
+    }
+    else{
+    resistencia1 = Math.ceil(Math.random()*5);
+    }
+    
+    life1 = resistencia1 * 5;
+    if(valor=0){
+        life1=1;
+    }
+}
+var p2 = (nome:caio){
+    valor=10;
+    forca = Math.ceil(Math.random()*5);
+    valor= valor-força;
+    armadura = Math.ceil(Math.random()*5);
+    valor= valor-armadura;
+    
+    if(valor<5){
+    resistencia = valor;
+    }
+    else{
+    resistencia = Math.ceil(Math.random()*5);
+    }
+    
+    life = resistencia * 5;
+    if(valor=0){
+        life=1;
+    }
+}
+
+fight = function (p1,p2){
+    while(life1<0 && life<0)
+{
+  life1= life1 -(forca*(random(1,6) - armadura1*(random(1,6));
+  if(lefi1>0){
+      life= life -(forca1*(random(1,6) - armadura*(random(1,6));
+  }
+ }
+if(life1>0) {return p1;}
+else    {return p2;}
+}*/
